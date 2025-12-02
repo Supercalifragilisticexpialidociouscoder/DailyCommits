2d8bb57be64bd1891386fed587e528e407071767
Supercalifragilisticexpialidociouscoder <abhiram200869@gmail.com>
2025-12-02 14:10:20 +0530

    updated

--- dec2.py
+++ dec2.py
@@ -11,3 +11,15 @@ c = child()
 c.mother_qualities()
 c.father_qualities()
 c.child_qualities()
+
+class animal:
+    def sound(self):
+        print("Animal makes a sound")
+class dog(animal):
+    def sound(self):
+        print("Dog barks")
+class baby_dog(dog):
+    def sound(self):
+        print("Baby dog yaps")
+b = baby_dog()
+b.sound()
