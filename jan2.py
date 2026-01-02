cdf51092c148246772bb00f0afc11de800e1d682
Supercalifragilisticexpialidociouscoder <abhiram200869@gmail.com>
2026-01-02 15:35:43 +0530

    i giveup

--- jan2.py
+++ jan2.py
@@ -1,26 +1,17 @@
 
-x = int(input("Enter number of rows: "))
-y  = int(input("Enter number of rows: "))
-a = int(input("Enter spaces:"))
-sum = 0
-for y in range(y, 1,-1 ):
-    y -= 1
-    sum += y
-    for j in range(a - y):
-        print(" ", end="")
-    for k in range(y):
-        print("* ", end="")
-    print()
+# x = int(input("Enter number of rows: "))
+# y  = int(input("Enter number of rows: "))
+# a = int(input("Enter spaces:"))
 
-sum = 0
-for i in range(x):
-    i += 1
-    sum += i
-    for j in range(a - i):
-        print(" ", end="")
-    for k in range(i):
-        print("* ", end="")
-    print()
+# sum = 0
+# for i in range(x):
+#     i += 1
+#     sum += i
+#     for j in range(a - i):
+#         print(" ", end="")
+#     for k in range(i):
+#         print("* ", end="")
+#     print()
 
 
 # sum = 0
@@ -32,3 +23,15 @@ for i in range(x):
 #     for k in range(y):
 #         print("* ", end="")
 #     print()
+
+x = int(input("Enter number of rows: "))
+a = int(input("Enter spaces:"))
+sum = 0
+for i in range(x):
+    i += 1
+    sum += i
+    for j in range(a - i):
+        print(" ", end="")
+    for k in range(i):
+        print("* ",)
+    print()
