f0b136e542bae0999b02f4a1f6a431704997e129
Supercalifragilisticexpialidociouscoder <abhiram200869@gmail.com>
2026-01-02 15:05:16 +0530

    Stars pattern

---
+++ jan2.py
@@ -0,0 +1,24 @@
+
+x = int(input("Enter number of rows: "))
+y  = int(input("Enter number of rows: "))
+a = int(input("Enter spaces:"))
+sum = 0
+for i in range(x):
+    i += 1
+    sum += i
+    for j in range(a - i):
+        print(" ", end="")
+    for k in range(i):
+        print("* ", end="")
+    print()
+
+
+sum = 0
+for y in range(y,0 ,-1):
+    y -= 1
+    sum += y
+    for j in range(a - y):
+        print(" ", end="")
+    for k in range(y):
+        print("* ", end="")
+    print()
