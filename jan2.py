68a2c029f0bdb1ee8aa33522eb05c9af265623ee
Supercalifragilisticexpialidociouscoder <abhiram200869@gmail.com>
2026-01-02 15:27:40 +0530

    Reverse version

--- jan2.py
+++ jan2.py
@@ -3,22 +3,32 @@ x = int(input("Enter number of rows: "))
 y  = int(input("Enter number of rows: "))
 a = int(input("Enter spaces:"))
 sum = 0
+for y in range(y, 1,-1 ):
+    y -= 1
+    sum += y
+    for j in range(a - y):
+        print(" ", end="")
+    for k in range(y):
+        print("* ", end="")
+    print()
+
+sum = 0
 for i in range(x):
     i += 1
     sum += i
     for j in range(a - i):
-        print(f"{" "} + {" "} + {" "}", end="" )
+        print(" ", end="")
     for k in range(i):
-        print(f'{"* "} + {"* "} + {"* "} ,end=""' )
+        print("* ", end="")
     print()
 
 
-sum = 0
-for y in range(y,0 ,-1):
-    y -= 1
-    sum += y
-    for j in range(a - y):
-        print(f"{" "} + {" "} + {" "}" ,end="" )
-    for k in range(y):
-        print(f'{"* "} + {"* "} + {"* "} ,end=""' )
-    print()
+# sum = 0
+# for y in range(y,0 ,-1):
+#     y -= 1
+#     sum += y
+#     for j in range(a - y):
+#         print(" ", end="")
+#     for k in range(y):
+#         print("* ", end="")
+#     print()
