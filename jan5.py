6498097bf0ba40a900a25eccaada1ec9e03d3345
Supercalifragilisticexpialidociouscoder <abhiram200869@gmail.com>
2026-01-05 21:39:28 +0530

    chess game p3

--- jan5.py
+++ jan5.py
@@ -78,3 +78,44 @@ class Bishop(Piece):
 class Knight(Piece):
     symbol = 'N'
 
+    def valid_moves(self, board, x, y):
+        moves = []
+        for dx, dy in [(2,1), (1,2), (-1,2), (-2,1), (-2,-1), (-1,-2), (1,-2), (2,-1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < 8 and 0 <= ny < 8:
+                if not board[ny][nx] or board[ny][nx].color != self.color:
+                    moves.append((nx, ny))
+        return moves
+
+class Pawn(Piece):
+    symbol = 'P'
+
+    def valid_moves(self, board, x, y):
+        moves = []
+        direction = -1 if self.color == 'w' else 1
+        # Move forward
+        if 0 <= y + direction < 8 and not board[y + direction][x]:
+            moves.append((x, y + direction))
+            # First move double
+            if (self.color == 'w' and y == 6) or (self.color == 'b' and y == 1):
+                if not board[y + 2 * direction][x]:
+                    moves.append((x, y + 2 * direction))
+        # Captures
+        for dx in [-1, 1]:
+            nx = x + dx
+            ny = y + direction
+            if 0 <= nx < 8 and 0 <= ny < 8 and board[ny][nx] and board[ny][nx].color != self.color:
+                moves.append((nx, ny))
+        return moves
+
+def create_board():
+    board = [[None for _ in range(8)] for _ in range(8)]
+    # Place pieces
+    for color, y_pawn, y_back in [('w', 6, 7), ('b', 1, 0)]:
+        for x in range(8):
+            board[y_pawn][x] = Pawn(color)
+        pieces = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+        for x, piece in enumerate(pieces):
+            board[y_back][x] = piece(color)
+    return board
+
