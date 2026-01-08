4ab54b7f878f71de01b12dcb1528557d8d731ff0
Abhi :) <abhiram200869@gmail.com>
2026-01-08 23:14:29 +0530

    Fix typo in qwertys function return string

--- qwertys.py
+++ qwertys.py
@@ -1,5 +1,5 @@
 #epic build
-def qwertys():    return "This is the qwertyys module."  
+def qwertys():    return "This is the qwertys module."  
 #epic test
 def test_qwertys():    assert qwertys() == "This is the qwertys module."
 #epic end
