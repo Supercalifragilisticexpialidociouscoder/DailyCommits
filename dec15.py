5f6f76d69b0d7b6df2829fc218fff6b6c32899b5
Abhi :) <abhiram200869@gmail.com>
2026-01-12 23:26:11 +0530

    Fix typo in a quote in random_quote function

--- dec15.py
+++ dec15.py
@@ -11,7 +11,7 @@ def random_quote():
     quotes = [
         "Code is like humor. When you have to explain it, it’s bad.",
         "First, solve the problem. Then, write the code.",
-        "Experience is the name everyone gives to their mistakes.",
+        "Experience is the name everyone gives to their mistakes..",
         "In order to be irreplaceable, one must always be different.",
         "Java is to JavaScript what car is to Carpet..."
     ]
